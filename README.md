86019f1a9d2c748afa48b3d541ac473c16755c38
bb84000 <bb84a@yahoo.fr>
2024-03-19 09:51:15 +0100

    Version 0.1.0.1
    Added MTS support

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mergets
  
+##TS/MTS Files merge
+
+Merge utility to join TS/MTS files parts recorded by some DVB adapters.
+
+Experimental: Please have a copy ofyour files before processing !
+
+Open source portable program, usage is free<br>
--- mergets1.pas
+++ mergets1.pas
@@ -41,6 +41,7 @@ type
     procedure BtnquitClick(Sender: TObject);
     procedure FormActivate(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
     procedure SBTSFilesClick(Sender: TObject);
   private
     UserPath: String;
@@ -71,6 +72,11 @@ begin
 
 end;
 
+procedure TFMergeTS.FormCreate(Sender: TObject);
+begin
+
+end;
+
 procedure TFMergeTS.BtnquitClick(Sender: TObject);
 begin
   close;
@@ -92,6 +98,7 @@ begin
     Memo1.Lines.add('Fichiers à fusioner :');
     Memo1.Lines.add(CBtsfiles.Items.Text);
     BtnMerge.Enabled:= true;
+
   end;
 end;
 
@@ -103,8 +110,15 @@ begin
 end;
 
 procedure TFMergeTS.BtnMergeClick(Sender: TObject);
+type
+  tstype = (TS, MTS);
 var
-  mypaq : array [0..187] of byte;
+  filtyp: tstype;
+  fhnd : THandle;
+  mypaq : array [0..191] of byte;   //192 for M2TS
+  paqsize: Integer;
+  paqofs: Integer;
+  //mypaq : array  of byte;   //192 for M2TS
   mypacket: TSPacket;
   pcrbase, lastpcr: Int64;
   tsread, tspos: Int64;
@@ -113,12 +127,15 @@ var
   tssize, filesize: Int64;
   beginpos, endpos : Int64;
   progress1, progress2 : LongInt;
-  fis: TMemoryStream;
+  //fis: TMemoryStream;
+  fts: TFileStream;
   fos: TFileStream;
   x, y: LongInt;
   i: integer;
   t: double;
+  StartTime, EndTime : TDatetime;
   hms: String;
+  Buf5: array [0..4] of Byte;
 begin
   if FileExists(EMergedTS.Text) then
   begin
@@ -132,6 +149,7 @@ begin
 
     end;
   end;
+  StartTime:= Now;
   fos:= TFileStream.Create(EMergedTS.Text, fmCreate);
   //mypacket:= TSPacket.Create;
   mypacket:= Default(TSPacket);
@@ -141,29 +159,50 @@ begin
   filesize:=0;
   ProgressBar2.position:= 0;
   progress2:= 0;
+  // check file type on first file
+  fhnd:= FileOpen(CBtsfiles.Items[0], fmOpenRead);
+  FileRead(fhnd, Buf5, 5 );
+  FileClose(fhnd);
+  if Buf5[0]= $47 then filtyp:= TS else
+  if Buf5[4]= $47 then filtyp:= MTS else
+  begin
+    ShowMessage('Format de fichier non supporté');
+    memo1.Append('Format de fichier non supporté');
+    exit;
+  end;
+  if filtyp = TS then
+    begin
+      paqsize:= 188;
+      paqofs:=0;
+    end else
+    begin
+      paqsize:= 192;
+      paqofs:= 4;
+      //EMergedTS.text:= ChangeFileExt(EMergedTS.text, '.mts');
+    end;
+    Application.ProcessMessages ;
   // Now, we process each TS file
   for y:= 0 to CBtsfiles.Items.Count-1 do
   begin
     beginpos:= 0;
     pcrbeg:= false;
-    memo1.lines.add('Analyse du fichier '+CBtsfiles.Items[y]);
+    memo1.lines.add('Copie du fichier '+CBtsfiles.Items[y]);
     Progressbar1.Position:= 0;
     tsread:= 0;
     tspos:= 0;
     // Open ts file
-    fis:= TMemoryStream.Create;
-    Fis.LoadFromFile(CBtsfiles.Items[y]);
-    tssize:= fis.Size div 188;
+    fts:= TFileStream.Create(CBtsfiles.Items[y], fmOpenRead) ;
+    tssize:= fts.Size div paqsize;
     for x:= 0 to tssize-1 do
     begin
       //fis.Position:= 0;
-      tsread:= fis.Read(mypaq, 188);
-      //ProgressBar1.Position:= Round(100*x / tssize);
-      //Progressbar2.position:= progress2+ Round(ProgressBar1.Position/CBtsfiles.Items.Count) ;
-      mypacket.adapfield:=  ((mypaq[3] and $0ff) and $030) shr 4;              //(mypaq[3]&0xff & 0x30) >> 4;
+      tsread:= fts.Read(mypaq, paqsize);
+      if (filtyp=TS) then
+      begin
+      mypacket.adapfield:=  ((mypaq[paqofs+3] and $0ff) and $030) shr 4;              //(mypaq[3]&0xff & 0x30) >> 4;
       if mypacket.adapfield > 1 then
       begin
-        mypacket.pcrflag:= (((mypaq[5] and $010) shr 4) <> 0);                 //(((mypaq[5] & 0x10) >> 4) != 0);
+        mypacket.pcrflag:= (((mypaq[paqofs+5] and $010) shr 4) <> 0);                 //(((mypaq[5] & 0x10) >> 4) != 0);
         if (mypacket.pcrflag) then
         begin
           endpos:= tspos;
@@ -175,8 +214,8 @@ begin
 	    tmp:= tmp or (mypaq[i] and $0FF);	  //tmp |= (mypaq[i] & 0xFF);
 	  end;
 	  mypacket.pcr:= tmp shr 7; //33 bits}
-          tmp:= (mypaq[6] and $0ff)* $1000000+(mypaq[7] and $0ff)* $10000+(mypaq[8] and $0ff)* $100+(mypaq[9] and $0ff) ;
-	  mypacket.pcr:= (tmp*$100+(mypaq[10] and $0ff)) shr 7; //33 bits
+          tmp:= (mypaq[paqofs+6] and $0ff)* $1000000+(mypaq[paqofs+7] and $0ff)* $10000+(mypaq[paqofs+8] and $0ff)* $100+(mypaq[paqofs+9] and $0ff) ;
+	  mypacket.pcr:= (tmp*$100+(mypaq[paqofs+10] and $0ff)) shr 7; //33 bits
           pcrbase:= mypacket.pcr;
           if ((y > 0) and (not pcrbeg) and (pcrbase = lastpcr)) then
           begin
@@ -185,31 +224,40 @@ begin
 	    mypacket.pcrtime:= (pcrbase div 90); // convert to milliseconds
             t := mypacket.pcrtime/ MSecsPerDay;
             hms:= FormatDateTime('[h]:nn:ss.zzz', t, [fdoInterval]);
-	    memo1.append ('Raccord du fichier'+CBtsfiles.Items[y]+' au PCR '+hms); //                   //tsfiles[y].getName()+" au PCR : "+hms+"\n";
+	    memo1.append ('Raccord du fichier'+CBtsfiles.Items[y]+' au PCR '+hms);       //tsfiles[y].getName()+" au PCR : "+hms+"\n";
 	  end;
 	end;
-
       end;
-      if ((y=0) or pcrbeg) then fos.write(mypaq, 188);         	// if (y==0 || pcrbeg) outChan.write(bpaq);
-      tspos:= tspos+tsread;                                             //	pos+= read;
-      ProgressBar1.Position:= Round(100*x / tssize);
-      Progressbar2.position:= progress2+ Round(ProgressBar1.Position/CBtsfiles.Items.Count) ;
+      end;
+      if ((y=0) or pcrbeg or (filtyp=MTS)) then fos.write(mypaq, paqsize);         	// if (y==0 || pcrbeg) outChan.write(bpaq);
+      tspos:= tspos+tsread;                                     //	pos+= read;
+      if  (x mod 200) = 0 then
+      begin
+        ProgressBar1.Position:= Round(100*x / tssize);                                          // use mod to update
+        Progressbar2.position:= progress2+ Round(ProgressBar1.Position/CBtsfiles.Items.Count) ;
+        Application.ProcessMessages;
+      end;
     end;
-    lastpcr:= pcrbase;
-    t := (lastpcr div 90)/ MSecsPerDay;
-    hms:= FormatDateTime('[h]:nn:ss.zzz', t, [fdoInterval]);
-    memo1.append('Dernier PCR du fichier '+CBtsfiles.Items[y]+': '+hms); //+ millitotime((int) (lastpcr/90))+"\n");
-    filesize:= filesize + endpos-beginpos;
-    //fos.Position:= filesize;					// on tronque le fichier
-    fos.Size:= filesize;					//outChan.position(filesize);
-					//outChan.truncate(filesize);
+    ProgressBar1.position:= 100;
+    if filtyp= TS then
+    begin
+      lastpcr:= pcrbase;
+      t := (lastpcr div 90)/ MSecsPerDay;
+      hms:= FormatDateTime('[h]:nn:ss.zzz', t, [fdoInterval]);
+      memo1.append('Dernier PCR du fichier '+CBtsfiles.Items[y]+': '+hms); //+ millitotime((int) (lastpcr/90))+"\n");
+      filesize:= filesize + endpos-beginpos;
+     // on tronque le fichier
+      fos.Size:= filesize;
+    end;
 
     progress2:= ProgressBar2.position;
-    if assigned(fis) then fis.Free;
+    if assigned(fts) then fts.Free;
   end;
-  //ProgressBar2.position:= 100;
+  ProgressBar2.position:= 100;
+  EndTime:= now;
+  memo1.Append('Fusion terminée en '+FormatDateTime('[h]:nn:ss.zzz', EndTime-StartTime, [fdoInterval]));
+  memo1.Append('Fichier fusionné : '+EMergedTS.Text);
 
-  //if assigned(mypacket) then mypacket.Free;
   if assigned(fos) then fos.Free;
 end;
 
